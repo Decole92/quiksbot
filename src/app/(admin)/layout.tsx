9f464bc5118d75ceb78f1654e04786f8129a860b
tudorache.leonard.dev <tudorache.leonard.dev@gmail.com>
2026-04-20 08:52:02 +0200

    again debug

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -30,8 +30,8 @@
 
 // export default Adminlayout;
 
-import Header from "@/components/Header";
-import Sidebar from "@/components/SideMenu/Sidebar";
+// import Header from "@/components/Header";
+// import Sidebar from "@/components/SideMenu/Sidebar";
 
 import { Toaster } from "@/components/ui/toaster";
 import React from "react";
@@ -39,9 +39,9 @@ import React from "react";
 function AdminLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <div className='min-h-screen bg-background'>
-      <Header />
+      {/* <Header /> */}
       <div className='flex w-full'>
-        <Sidebar />
+        {/* <Sidebar /> */}
         <main className='flex-1'>
           <div className='w-full md:min-h-screen h-full flex flex-col pl-16 md:pl-4'>
             {children}
