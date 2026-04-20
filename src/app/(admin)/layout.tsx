94c824df6fb030de5edef8929ab3d8b61f5d990f
tudorache.leonard.dev <tudorache.leonard.dev@gmail.com>
2026-04-20 09:03:23 +0200

    added combug

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -30,7 +30,7 @@
 
 // export default Adminlayout;
 
-// import Header from "@/components/Header";
+import Header from "@/components/Header";
 import Sidebar from "@/components/SideMenu/Sidebar";
 
 import { Toaster } from "@/components/ui/toaster";
@@ -39,9 +39,9 @@ import React from "react";
 function AdminLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <div className='min-h-screen bg-background'>
-      {/* <Header /> */}
+      <Header />
       <div className='flex w-full'>
-        <Sidebar />
+        {/* problem with this <Sidebar /> */}
         <main className='flex-1'>
           <div className='w-full md:min-h-screen h-full flex flex-col pl-16 md:pl-4'>
             {children}
