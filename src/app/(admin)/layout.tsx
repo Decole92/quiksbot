cda11fecd17c30afa47524d79586a282784d83c2
tudorache.leonard.dev <tudorache.leonard.dev@gmail.com>
2026-04-20 07:41:58 +0200

    debug layout

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,31 +1,64 @@
+// import Header from "@/components/Header";
+// import Sidebar from "@/components/SideMenu/Sidebar";
+// import { Toaster } from "@/components/ui/toaster";
+
+// import { ClerkLoaded, RedirectToSignIn } from "@clerk/nextjs";
+// import { auth } from "@clerk/nextjs/server";
+
+// import React from "react";
+
+// async function Adminlayout({
+//   children,
+// }: Readonly<{ children: React.ReactNode }>) {
+//   return (
+//     <ClerkLoaded>
+//       <div className='min-h-screen bg-background'>
+//         <Header />
+
+//         <div className='flex w-full'>
+//           <Sidebar />
+//           <main className='flex-1 '>
+//             <div className='w-full md:min-h-screen h-full flex flex-col pl-16 md:pl-4'>
+//               {children}
+//             </div>
+//           </main>
+//         </div>
+//       </div>
+//     </ClerkLoaded>
+//   );
+// }
+
+// export default Adminlayout;
 import Header from "@/components/Header";
 import Sidebar from "@/components/SideMenu/Sidebar";
 import { Toaster } from "@/components/ui/toaster";
-
-import { ClerkLoaded, RedirectToSignIn } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
-
+import { RedirectToSignIn } from "@clerk/nextjs";
 import React from "react";
 
-async function Adminlayout({
+async function AdminLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <ClerkLoaded>
-      <div className='min-h-screen bg-background'>
-        <Header />
+  const { userId } = await auth();
 
-        <div className='flex w-full'>
-          <Sidebar />
-          <main className='flex-1 '>
-            <div className='w-full md:min-h-screen h-full flex flex-col pl-16 md:pl-4'>
-              {children}
-            </div>
-          </main>
-        </div>
+  if (!userId) {
+    return <RedirectToSignIn />;
+  }
+
+  return (
+    <div className='min-h-screen bg-background'>
+      <Header />
+      <div className='flex w-full'>
+        <Sidebar />
+        <main className='flex-1'>
+          <div className='w-full md:min-h-screen h-full flex flex-col pl-16 md:pl-4'>
+            {children}
+          </div>
+        </main>
       </div>
-    </ClerkLoaded>
+      <Toaster />
+    </div>
   );
 }
 
-export default Adminlayout;
+export default AdminLayout;
